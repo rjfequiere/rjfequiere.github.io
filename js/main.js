4dcf9fc0b24120b92259081d163e570fc2733986
Cameron Spickert <cspickert@gmail.com>
2018-04-15 00:34:52 -0400

    Add Lately page

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ $(function() {
     prefetch: true,
     cacheLength: 4
   });
+  var $grid = $('.lately-grid').masonry({
+    itemSelector: '.lately-grid-item',
+    columnWidth: '.lately-grid-item-sizer'
+  });
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+  });
 });

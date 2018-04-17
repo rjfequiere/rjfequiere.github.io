8b71f6d3ab67918ff75e4efa1f7833adc7a37330
Cameron Spickert <cspickert@gmail.com>
2018-04-17 00:34:24 -0400

    Fix masonry layout behavior

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(function() {
   'use strict';
-  $('#main').smoothState({
-    prefetch: true,
-    cacheLength: 4
-  });
   var $grid = $('.lately-grid').masonry({
     itemSelector: '.lately-grid-item',
     columnWidth: '.lately-grid-item-sizer'
   });
-  $grid.imagesLoaded().progress( function() {
+  $grid.imagesLoaded().progress(function() {
     $grid.masonry('layout');
+  });
+  $('#main').smoothState({
+    prefetch: true,
+    cacheLength: 4,
+    onAfter: function () {
+      $grid.masonry('layout');
+    }
   });
 });
